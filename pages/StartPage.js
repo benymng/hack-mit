8c40a1140161bc899c561577fd67139fcead39f5
John Atkinson <johnatkinson2020@gmail.com>
2023-09-16 18:22:23 -0400

    Working on home and start screen

---
+++ pages/StartPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function StartPage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page of our multi-page app.</p>
+    </div>
+  );
+}
+
+export default StartPage;
